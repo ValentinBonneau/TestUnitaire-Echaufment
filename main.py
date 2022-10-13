685278931848dfe879ea0fa3c86ba3ba1d9b0e1d
ValentinBonneau <valentin.bonneau@epsi.fr>
2022-10-13 10:01:05 +0200

    feat: lang

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import datetime
 
+import locale
+import ctypes
+
+
+windll = ctypes.windll.kernel32
+lang = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
+
+
+
+print(lang);
+
 heure = datetime.datetime.now().hour
 
 bonjour = ""
 aurevoir = ""
 
-if 5 <= heure < 18:
-    bonjour = "Bonjour"
-else:
-    bonjour = "Bonsoir"
+if lang == "fr_FR":
+    if 5 <= heure < 18:
+        bonjour = "Bonjour"
+    else:
+        bonjour = "Bonsoir"
 
-if 5 <= heure < 12:
-    aurevoir = "Bonne fin de matinée"
-elif 12 <= heure < 18:
-    aurevoir = "Bonne fin d'après midi"
-elif heure < 2 or 18 <= heure:
-    aurevoir = "Bonne fin d'après midi"
+    if 5 <= heure < 12:
+        aurevoir = "Bonne fin de matinée"
+    elif 12 <= heure < 18:
+        aurevoir = "Bonne fin d'après midi"
+    elif heure < 2 or 18 <= heure:
+        aurevoir = "Bonne soirée"
+    else:
+        aurevoir = "Bonne nuité les ptiots"
 else:
-    aurevoir = "Bonne nuité les ptiots"
+    if 5 <= heure < 18:
+        bonjour = "Good Morning"
+    else:
+        bonjour = "Good Afternoon"
+
+    if 5 <= heure < 12:
+        aurevoir = "good late morning"
+    elif 12 <= heure < 18:
+        aurevoir = "good late Afternoon"
+    elif heure < 2 or 18 <= heure:
+        aurevoir = "good evening"
+    else:
+        aurevoir = "good night"
 
 print(bonjour, "et bienvenue sur le programme d'échaufment de Valentin Bonneau")
 
